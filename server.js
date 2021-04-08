6504a885ff6d4bcf439fa7c37d899c4a311ab200
Pratik Karki <pratikkarki5@gmail.com>
2021-04-08 21:15:27 +0545

    Fix small error regarding port

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ dotenv.config({ path: './config.env' }); //Loads .env file contents into process
 const app = require('./app');
 
 //1. CONNECT TO DATABASE
-const dbConnectionString = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const dbConnectionString = process.env.DATABASE;
 mongoose.connect(dbConnectionString, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true, useUnifiedTopology: true })
 .then(() => console.log('DB connection successful.'));
 
 //2. START SERVER
-const port = process.env.port || 7000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
