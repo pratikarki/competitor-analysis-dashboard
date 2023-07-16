51777f234766733012e0b0dcf29ff4921e1ab3cf
Pratik Karki <pratikkarki5@gmail.com>
2023-07-16 09:36:39 +0545

    remove unnecessary packages

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Competitor Analysis Dashboard is a web application that provides detailed information about different websites to its user. The insight information that are publicly available through different sources are gathered, stored and displayed accordingly. It integrates web scraping and API calls as its main source of data which allows the application to collect and store data in MongoDB database. Information includes bounce rates, search traffic, visit duration, organic searches and clicks, website’s ranks, related keywords, backlinks, visitors by country, top pages and much more. The user gets to analyse and compare this information to their benefit. It is designed especially for online businesses/ websites to analyse and compare their data with their competition.
 
 To login, use these credentials: (please note that these are just dummy email addresses and use these responsibly)
+
 - pratik.karki@gmail.com (ADMIN)
 - binod.shakya@gmail.com (ADMIN)
 - sushant.ghimire@gmail.com (ADMIN)
@@ -20,3 +21,9 @@ To login, use these credentials: (please note that these are just dummy email ad
 - shresthasunil859@gmail.com (onlinekhabar.com)
 
 Password for all these accounts: pass1234
+
+\***\* REMOVED PACKAGES \*\***
+"multer": "^1.4.2",
+"multer-s3-transform": "^2.10.3",
+"sharp": "^0.28.1",
+"slugify": "^1.4.6",
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,9 @@ const factory = require('./factoryHandler');
 const AppError = require('../utils/appError');
 
 const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3-transform');
-const sharp = require('sharp');
+// const multer = require('multer');
+// const multerS3 = require('multer-s3-transform');
+// const sharp = require('sharp');
 // const catchAsync = require('../utils/catchAsync');
 
 const s3 = new aws.S3({
@@ -13,38 +13,38 @@ const s3 = new aws.S3({
   secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
 });
 
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image')) {
-    cb(null, true);
-  }
-  else {
-    const err = new AppError('Not an image, Please upload an image file', 400);
-    cb(err, false);
-  }
-}
+// const multerFilter = (req, file, cb) => {
+//   if (file.mimetype.startsWith('image')) {
+//     cb(null, true);
+//   }
+//   else {
+//     const err = new AppError('Not an image, Please upload an image file', 400);
+//     cb(err, false);
+//   }
+// }
 
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.AWS_BUCKET,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: 'public-read',
-    shouldTransform: function (req, file, cb) {
-      cb(null, /^image/i.test(file.mimetype))
-    },
-    transforms: [{
-      key: function(req, file, cb) {
-        cb(null, `user-${req.user.id}-${Date.now()}`)
-      },
-      transform: function (req, file, cb) {
-        cb(null, sharp().resize(500, 500))
-      }
-    }]
-  }),
-  fileFilter: multerFilter
-});
+// const upload = multer({
+//   storage: multerS3({
+//     s3: s3,
+//     bucket: process.env.AWS_BUCKET,
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     acl: 'public-read',
+//     shouldTransform: function (req, file, cb) {
+//       cb(null, /^image/i.test(file.mimetype))
+//     },
+//     transforms: [{
+//       key: function(req, file, cb) {
+//         cb(null, `user-${req.user.id}-${Date.now()}`)
+//       },
+//       transform: function (req, file, cb) {
+//         cb(null, sharp().resize(500, 500))
+//       }
+//     }]
+//   }),
+//   fileFilter: multerFilter
+// });
 
-exports.uploadPhoto = upload.single('photo');
+// exports.uploadPhoto = upload.single('photo');
 
 // const bufferStorage = multer.memoryStorage();
 
