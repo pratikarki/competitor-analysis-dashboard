873e972b9b61e5b3146085a61dbea539d7fbf1e1
Pratik Karki <pratikkarki5@gmail.com>
2023-07-16 16:42:41 +0545

    modified  vercel.json

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ export const login = async (email, password) => {
 	try {
 		const res = await axios({
 			method: 'POST',
-			url: '/api/v1/users/login',
+			url: '/api/v1/users/login/',
 			data: {
 				email: email,
 				password: password,
